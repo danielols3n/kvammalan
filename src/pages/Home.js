bb0dabf1a5f058681f308f2e60217e27f9fd7771
danielols3n <73017930+danielols3n@users.noreply.github.com>
2022-04-17 19:46:26 +0200

    Continued on homepage, added all necessary info and added "hugseliste"

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Button, Card, Container, NavLink, Row } from "react-bootstrap";
 import NavbarComponent from "../components/navbar/Navbar";
 import "../css/Home.css";
-import { IoMdTime } from 'react-icons/io'
+import { IoMdTime, IoMdPricetag } from 'react-icons/io'
+import { MdOutlinePlace } from 'react-icons/md'
 
 function Home() {
   return(
@@ -37,11 +38,11 @@ function Home() {
             </div>
           </Container>  
           <div className="divider"></div>
-          <Container className="d-flex flex-column" fluid>
-            <h1 className="fw-bolder display-5 mt-5 align-self-center">INFORMASJON</h1>
-            <Container fluid className="d-flex">
-              <Row lg={3} md={3} sm={1} className="d-flex justify-content-center">
-                <Card className="col-lg-2 m-2" bg='dark'>
+          <Container className="d-flex flex-column m-0 p-0" fluid>
+            <h1 className="fw-bolder display-5 mt-5 align-self-center mb-5">INFORMASJON</h1>
+            <Container fluid className="d-flex m-0 p-0 mb-5">
+              <Row className="d-flex justify-content-center m-auto p-0">
+                <Card className="col-lg-2 mx-2" bg='dark'>
                   <Card.Header className="d-flex justify-content-center">
                     <IoMdTime size='2.5rem' />
                   </Card.Header>
@@ -55,32 +56,64 @@ function Home() {
                     </div>
                   </Card.Body>
                 </Card>
-                <Card className="col-lg-2 m-2">
+                <Card bg='dark' className="col-lg-2 mx-2">
                   <Card.Header className="d-flex justify-content-center">
-                    <IoMdTime size='2.5rem' />
+                    <MdOutlinePlace size='2.5rem' />
                   </Card.Header>
-                  <Card.Body>
-                    <b>JuniorLAN:</b>&nbsp;13.06.2022 kl. 14:00 til 13.06.2022 kl. 19:00
+                  <Card.Body className="d-flex flex-column">
+                    <b style={{ alignSelf: 'center', fontSize: '1.1rem' }}>Kvam ungdomsskule</b>
+                    <p className="m-0 mt-3">Holmatunvegen 12</p>
+                    <p>5610 Øystese</p>
+                    <small>
+                      Me held til i fyrste etasje på Kvam ungdomsskule.
+                      Inngangen finn du på framsida av skule
+                      (den sida med sandvolleyballbanen).
+                    </small>
                   </Card.Body>
                 </Card>
-                <Card className="col-lg-2 m-2">
+                <Card bg='dark' className="col-lg-2 mx-2">
                   <Card.Header className="d-flex justify-content-center">
-                    <IoMdTime size='2.5rem' />
+                    <IoMdPricetag size='2.5rem' />
                   </Card.Header>
                   <Card.Body>
-                    <b>JuniorLAN:</b>&nbsp;13.06.2022 kl. 14:00 til 13.06.2022 kl. 19:00
+                    <b>JuniorLAN</b>
+                    <br></br>
+                    <p className="mt-2">kr 50 ,-</p>
+                    <br></br>
+                    <b>SeniorLAN</b>
+                    <br></br>
+                    <ul style={{ listStyleType: 'circle' }}>
+                      <li>Ikkje medlem: kr 150 ,-</li>
+                      <li>Medlem: kr 100 ,-</li>
+                    </ul>
                   </Card.Body>
                 </Card>
-                <Card className="col-lg-2 m-2">
+                <Card bg='dark' className="col-lg-2 mx-2">
                   <Card.Header className="d-flex justify-content-center">
-                    <IoMdTime size='2.5rem' />
+                    <h3 className="fw-bolder">Det vert</h3>
                   </Card.Header>
                   <Card.Body>
-                    <b>JuniorLAN:</b>&nbsp;13.06.2022 kl. 14:00 til 13.06.2022 kl. 19:00
+                    <ul style={{ listStyleType: 'circle' }}>
+                      <li>Kiosk</li>
+                      <li>Konkurransar</li>
+                      <li>God stemning</li>
+                    </ul>
                   </Card.Body>
                 </Card>
               </Row>
             </Container>
+          </Container>
+          <div className="divider"></div>
+          <Container fluid className="d-flex flex-column m-0 p-0 mt-3">
+            <h2 className="mx-5 mt-3 fw-bolder">Hugseliste</h2>
+            <ul className="todo-list" style={{ listStyleType: 'circle' }}>
+              <li>Noko å spela på (konsoll, stasjonær pc, laptop etc.)</li>
+              <li>Skjerm(ar)</li>
+              <li>Alt av nødvendig utstyr for å spela (tastatur, mus, headset, musematte, ladarar etc.)</li>
+              <li>Nettverkskabel (nokre kan sitja så langt som 15 meter vekke frå næraste nettverkspunkt)</li>
+              <li>Stol (om du ynskjer. Me har stolar som du òg kan bruka)</li>
+              <li>Skøyteleidning(ar)</li>
+            </ul>
           </Container>
       </Container>
   )
