53fb7f88f9cc2d8e0648fc138413893d1b48b78b
danielols3n <olsen.daniel04@gmail.com>
2022-04-20 20:16:57 +0200

    Fixed db logic with next event on Event page

--- src/pages/Event.js
+++ src/pages/Event.js
@@ -3,24 +3,43 @@ import { Container } from 'react-bootstrap'
 import '../css/Event.css'
 import NavbarComponent from '../components/navbar/Navbar'  
 import Footer from '../components/footer/Footer' 
-import { collection, query, where } from 'firebase/firestore'
+import { collection, getDocs, query, where } from 'firebase/firestore'
 import { db } from '../firebase/config'
 
 function Event() {
-  const [eventData, setEvent] = useState(null)
-  const [data, setData] = useState(null)
+  const nextEventId = ' m8T5RrE1DAKWDSnHmoDh'
+  const [event, setEvent] = useState(null)
 
   useEffect(() => {
-    setData(JSON.parse(event))
+    const fetchEvent = async () => {
+      const temp = []
+      const q = query(collection(db, 'hendingar'), where('eventId', '==', nextEventId))
 
-    const q = query(collection(db, 'hendingar'), where('eventId', '==', data.nextevent))
+      const querySnapshot = await getDocs(q)
+
+      querySnapshot.forEach((doc) => {
+        temp.push({
+          title: doc.data().title, 
+          eventId: doc.data().eventId,
+          desc: doc.data().description,
+          start: doc.data().start,
+          end: doc.data().end,
+          img: doc.data().img,
+          prices: doc.data().prices,
+          place: doc.data().place
+        })
+      })
+      setEvent(temp[0])
+    }
+
+    fetchEvent()
   }, [])
 
   return (
     <Container fluid className="event d-flex flex-column p-0 m-0">
         <NavbarComponent data={{ background: 'transparent' }} />
-          <Container fluid className="d-flex flex-column">
-            <h1 className='fw-bolder mt-3 align-self-center'>Test</h1>
+          <Container fluid className="d-flex flex-column text-light">
+            {event !== null ? <h1 className='fw-bolder mt-3 align-self-center'>{event.title}</h1> : null}
           </Container>
         <Footer />
     </Container>
