84aaa51e1995fe13b615acb5b353b9fbf24fa9da
danielols3n <olsen.daniel04@gmail.com>
2022-04-28 12:28:09 +0200

    Finished regs page

--- src/pages/Registrations.js
+++ src/pages/Registrations.js
@@ -4,11 +4,13 @@ import { auth, db } from '../firebase/config'
 import { onAuthStateChanged } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
 import { collection, getDocs } from 'firebase/firestore'
+import { AiOutlineDelete } from 'react-icons/ai'
+import { BsEye } from 'react-icons/bs'
 
 function Registrations() {
     const nextEventId = 'BX4GwZjW1hTBn2G8NFwz'
     const [user, setUser] = useState(null)
-    const regs = []
+    const [regs, setRegs] = useState([])
     const navigate = useNavigate()
 
     useEffect(() => {
@@ -20,32 +22,37 @@ function Registrations() {
             }
         })
 
+        const fetchRegs = async () => {
             const colRef = collection(db, 'hendingar', nextEventId, 'registreringar')
-            
-            getDocs(colRef).then((snap) => {
-                snap.forEach((doc) => {
-                    regs.push({
-                        name: doc.data().name, 
-                        email: doc.data().email, 
-                        phone: doc.data().phone,
-                        birth: doc.data().birthdate,
-                        minor: doc.data().minor,
-                        parentemail: doc.data().parentemail,
-                        parentname: doc.data().parentname,
-                        parentphone: doc.data().parentphone, 
-                        paid: doc.data().paid,
-                        id: doc.id
-                    })
-                })
-            })
+             
+            const snap = await getDocs(colRef)
 
-        console.log(regs)
+            const temp = []
+            
+            snap.forEach((doc) => {
+                temp.push({
+                    name: doc.data().name, 
+                    email: doc.data().email, 
+                    phone: doc.data().phone,
+                    birth: doc.data().birthdate,
+                    minor: doc.data().minor,
+                    parentemail: doc.data().parentemail,
+                    parentname: doc.data().parentname,
+                    parentphone: doc.data().parentphone, 
+                    paid: doc.data().paid,
+                    id: doc.id
+                })
+                setRegs([...temp])
+            })
+        }
+
+        fetchRegs()
     }, [])
 
   return (
     <Container fluid className="d-flex flex-column p-0 m-0">
         <h1 className="align-self-center fw-bolder mt-3">Påmeldingar</h1>
-        <Table className="mt-5" striped bordered>
+        <Table style={{ width: '95%' }} className="mt-5 align-self-center" striped bordered>
             <thead>
                 <tr>
                     <th>ID</th>
@@ -57,19 +64,27 @@ function Registrations() {
                 </tr>
             </thead>
             <tbody>
-                {regs.map((item) => {
-                    console.log(item)
-                    return (
-                        <tr className="tablerow">
-                            <td>{item.id}</td>
-                            <td>{item.name}</td>
-                            <td>{item.email}</td>
-                            <td>{item.phone}</td>
-                            <td>{item.minor === true ? 'Ja' : 'Nei'}</td>
-                            <td>Handlingar</td>
-                        </tr>
-                    )
-                })}
+                {regs.length !== 0 ? 
+                <>
+                    {regs.map((item) => {
+                        console.log(item)
+                        return (
+                            <tr className="tablerow">
+                                <td>{item.id}</td>
+                                <td>{item.name}</td>
+                                <td>{item.email}</td>
+                                <td>{item.phone}</td>
+                                <td>{item.minor === true ? 'Ja' : 'Nei'}</td>
+                                <td className="d-flex justify-content-center">
+                                    <BsEye title='View' style={{ cursor: 'pointer', margin: 'auto' }} size="1.5rem" />
+                                    <AiOutlineDelete title='Delete' style={{ cursor: 'pointer', margin: 'auto' }} color='red' size="1.5rem" />
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </>
+                : null
+                }
             </tbody>
         </Table>
     </Container>
