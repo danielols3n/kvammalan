8f4798d15971a8ebb1faae4d8c30630906847b0a
Daniel Olsen <olsen.daniel04@gmail.com>
2022-11-18 23:05:15 +0100

    Continued on ViewEvent

--- src/pages/ViewEvent.js
+++ src/pages/ViewEvent.js
@@ -5,10 +5,11 @@ import Footer from '../components/footer/Footer'
 import { TailSpin } from 'react-loader-spinner'
 import { doc, getDoc } from 'firebase/firestore'
 import { db } from '../Firebase'
-import eventimage from '../img/event.jpg'
+import eventimage from '../img/event.png'
 import moment from 'moment'
 import 'moment/locale/nn'
 import { IoMdTime } from 'react-icons/io'
+import { MdOutlinePlace } from 'react-icons/md'
 
 function ViewEvent() {
   const [loading, setLoading] = useState(true)
@@ -58,12 +59,12 @@ function ViewEvent() {
                     <h1 className="text-light fw-bolder mt-3">{event.name.toUpperCase()}</h1>
                     <Container fluid className="d-flex flex-column align-items-center">
                         <Container fluid className="d-flex mt-3">
-                            {event.image !== undefined && event.image !== '' && event.image !== null ? <img alt='' style={{ width: '75%', height: 'auto', objectFit: 'contain', margin: 'auto' }} src={event.image} /> : <img alt='' style={{ width: '75%', height: 'auto', objectFit: 'contain', margin: 'auto' }} src={eventimage} />}
+                            {event.image !== undefined && event.image !== '' && event.image !== null ? <img alt='' style={{ width: '50%', height: 'auto', objectFit: 'contain', margin: 'auto' }} src={event.image} /> : <img alt='' style={{ width: '50%', height: 'auto', objectFit: 'contain', margin: 'auto' }} src={eventimage} />}
                         </Container>
                         <Form style={{ marginLeft: '2.5%' }} className="w-100">
                             <Form.Group as={Row} className="w-75 text-light fw-bolder mt-5">
                                 <Form.Label style={{ fontSize: '1.5rem' }} >BESKRIVELSE</Form.Label>
-                                <Form.Control style={{ fontSize: '0.9rem', resize: 'none' }} disabled plaintext className="text-light" defaultValue={event.description} as='textarea' rows={16} />
+                                <Form.Control style={{ fontSize: '0.9rem', resize: 'none' }} disabled plaintext className="text-light" defaultValue={event.description} as='textarea' rows={15} />
                             </Form.Group>
                         </Form>
                         <Form style={{ marginLeft: '2.5%' }} className="w-100">
@@ -79,6 +80,13 @@ function ViewEvent() {
                                 <Container fluid className="d-flex align-items-center mb-3 p-0">
                                     <IoMdTime size="1.5rem" />
                                     <Form.Control style={{ fontSize: '0.9rem', marginLeft: '1%', margin: '0' }} disabled plaintext className="text-light" defaultValue={moment.unix(new Date(event.endTime)/1000).locale('nn').format('LLL')} />
+                                </Container>
+                            </Form.Group>
+                            <Form.Group as={Row} className="w-75 text-light fw-bolder mt-2">
+                                <Form.Label className="my-0" style={{ fontSize: '1.5rem' }} >STAD</Form.Label>
+                                <Container fluid className="d-flex align-items-center mb-3 p-0">
+                                    <MdOutlinePlace size="1.5rem" />
+                                    <a style={{ cursor: 'pointer', marginLeft: '1%', margin: '0' }} href={`https://maps.google.com/?q=${event.address}`}><Form.Control style={{ fontSize: '0.9rem', marginLeft: '1%', margin: '0', cursor: 'pointer' }} disabled plaintext className="text-light" defaultValue={event.place} /></a>
                                 </Container>
                             </Form.Group>
                         </Form>
