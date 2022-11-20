4438b7f5e09a9cc76cf97a7852ede6cfa15fbf5d
Daniel Olsen <olsen.daniel04@gmail.com>
2022-11-20 22:17:58 +0100

    .

--- src/pages/ViewEvent.js
+++ src/pages/ViewEvent.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Container, Form, Row } from 'react-bootstrap'
+import { Card, Container, Form, Row } from 'react-bootstrap'
 import NavbarComponent from '../components/navbar/Navbar'
 import Footer from '../components/footer/Footer'
 import { TailSpin } from 'react-loader-spinner'
@@ -10,6 +10,8 @@ import moment from 'moment'
 import 'moment/locale/nn'
 import { IoMdTime } from 'react-icons/io'
 import { MdOutlinePlace } from 'react-icons/md'
+import ReactLinkify from 'react-linkify'
+import ReactAvatar from 'react-avatar'
 
 function ViewEvent() {
   const [loading, setLoading] = useState(true)
@@ -30,16 +32,18 @@ function ViewEvent() {
                 description: item.data().description, 
                 startTime: item.data().startTime, 
                 endTime: item.data().endTime,
-                image: null,
+                image: item.data().image,
                 tickets: item.data().tickets,
                 place: item.data().place,
                 address: item.data().address, 
                 id: item.id
-        })
+            })
+            console.log(item.data().tickets)
         }).catch(error => console.error(error))
     })
 
   document.title = 'Vis hending | KvammaLAN'
+
   return (
     <Container style={loading === false ? { minHeight: '100vh' } : { overflow: 'hidden', position: 'fixed' }} fluid className="d-flex flex-column p-0 m-0 faq">
       {loading === true ?
@@ -59,12 +63,16 @@ function ViewEvent() {
                     <h1 className="text-light fw-bolder mt-3">{event.name.toUpperCase()}</h1>
                     <Container fluid className="d-flex flex-column align-items-center">
                         <Container fluid className="d-flex mt-3">
-                            {event.image !== undefined && event.image !== '' && event.image !== null ? <img alt='' style={{ width: '50%', height: 'auto', objectFit: 'contain', margin: 'auto' }} src={event.image} /> : <img alt='' style={{ width: '50%', height: 'auto', objectFit: 'contain', margin: 'auto' }} src={eventimage} />}
+                            {event.image !== undefined && event.image !== '' && event.image !== null ? <img alt='' className="rounded" style={{ width: '50%', height: 'auto', objectFit: 'contain', margin: 'auto' }} src={event.image} /> : <img alt='' style={{ width: '50%', height: 'auto', objectFit: 'contain', margin: 'auto' }} src={eventimage} />}
                         </Container>
                         <Form style={{ marginLeft: '2.5%' }} className="w-100">
-                            <Form.Group as={Row} className="w-75 text-light fw-bolder mt-5">
-                                <Form.Label style={{ fontSize: '1.5rem' }} >BESKRIVELSE</Form.Label>
-                                <Form.Control style={{ fontSize: '0.9rem', resize: 'none' }} disabled plaintext className="text-light" defaultValue={event.description} as='textarea' rows={15} />
+                            <Form.Group as={Row} className="w-75 text-light mt-5">
+                                <Form.Label className="fw-bolder" style={{ fontSize: '1.5rem', marginLeft: '1.5%' }} >BESKRIVELSE</Form.Label>
+                                <ReactLinkify>
+                                    <p style={{ whiteSpace: 'pre-line' }} className="text-light">
+                                        {event.description}
+                                    </p>
+                                </ReactLinkify>
                             </Form.Group>
                         </Form>
                         <Form style={{ marginLeft: '2.5%' }} className="w-100">
@@ -90,6 +98,22 @@ function ViewEvent() {
                                 </Container>
                             </Form.Group>
                         </Form>
+                        <Container fluid className="d-flex flex-column mt-5 mb-3">
+                            <h2 className="fw-bolder text-light">BILLETTAR | {event.name}</h2>
+                            <Row className="w-100 d-flex justify-content-center mt-4 mb-5">
+                                {event.tickets.map((item) => {
+                                    return(
+                                        <Card className="d-flex flex-column m-3 col-lg-3 align-items-center p-3">
+                                            <ReactAvatar className="mt-2" name={item.ticketname} round maxInitials={2} />
+                                            <h2 className="fw-bolder mt-3">{item.ticketname.toUpperCase()}</h2>
+                                            <Container fluid className="d-flex justify-content-center">
+                                                <span style={{ fontSize: '1.2rem' }} className="mt-3"><b>PRIS:</b>&nbsp;{item.price} ,-</span>
+                                            </Container>
+                                        </Card>
+                                    )
+                                })}
+                            </Row>
+                        </Container>
                     </Container>
                 </>
             : null}
