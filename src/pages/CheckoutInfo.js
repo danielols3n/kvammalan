5e6d8b44e3d7b213ccc67ae4ab1fe28850d2b3ad
danielols3n <73017930+danielols3n@users.noreply.github.com>
2023-07-22 14:31:37 +0200

    E komt te betalingssido

--- src/pages/CheckoutInfo.js
+++ src/pages/CheckoutInfo.js
@@ -3,9 +3,10 @@ import { Container, Form, Row, Col, Button } from 'react-bootstrap'
 import NavbarComponent from '../components/navbar/Navbar' 
 import Footer from '../components/footer/Footer'
 import '../css/CheckoutInfo.css'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import { db } from '../Firebase'
-import { collection, addDoc } from 'firebase/firestore'
+import { collection, addDoc, updateDoc } from 'firebase/firestore'
+import { v4 as uuidv4 } from 'uuid'
 
 function CheckoutInfo() {
   const [name, setName] = useState('')
@@ -27,7 +28,8 @@ function CheckoutInfo() {
   const [parent2_phone, setParent2Phone] = useState('')
 
   const navigate = useNavigate()
-  const [validated, setValidated] = useState(false)
+  const [searchParams] = useSearchParams()
+  const [validate, setValidate] = useState(false)
 
   const [fortnite, setFortnite] = useState('')
   const [csgo, setCsgo] = useState('')
@@ -38,19 +40,51 @@ function CheckoutInfo() {
   const [lol, setLol] = useState('')
 
   const submit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkvalidity() === false) {
-      event.preventDefault()
-      event.stopPropagation()
+    event.preventDefault()
+
+    if (name === '' || phone === '' || email === '' || address === '' || zipcode === '' || city === '' || country === '' || birthdate === '' || parent1_email === '' || parent1_name === '' || parent1_phone === '' || birthdate === ' ' || gender === '') {
+      setValidate(true)
+      window.scrollTo(0, 0)
+      console.log(validate)
+    } else {
+      setValidate(false)
+      const id = uuidv4()
+
+      const colRef = collection(db, 'events', 'kvammalan2023', 'registrations')
+
+      addDoc(colRef, {
+        name: name, 
+        phone: phone, 
+        email: email,
+        address: address,
+        zipcode: zipcode, 
+        city: city, 
+        country: country,
+        birthdate: birthdate, 
+        gender: gender, 
+        selectedGames: selectedGames,
+        fortnite: fortnite, 
+        csgo: csgo, 
+        valorant: valorant,
+        fh5: fh5, 
+        mc: mc, 
+        rl: rl,
+        lol: lol,
+        parent1_name: parent1_name, 
+        parent1_email: parent1_email,
+        parent1_phone: parent1_phone,
+        parent2_name: parent2_name, 
+        parent2_email: parent2_email, 
+        parent2_phone: parent2_phone,
+        reservationId: id,
+        seatId: searchParams.get('seatId'),
+        ticketId: searchParams.get('ticketId')
+      }).then(() => {
+        navigate(`/kvammalan/checkout/payment?ticketId=${searchParams.get('ticketId')}&seatId=${searchParams.get('seatId')}&reservationId=${id}`)
+      })
     }
 
-    setValidated(true)
-
-    const colRef = collection(db, 'events', 'kvammalan2023', 'registrations')
-
-    addDoc(colRef, {
-      
-    })
+    
   }
 
   const games = ['Fortnite', 'Counter-Strike: Global Offensive', 'Valorant', 'Forza Horizon 5', 'Minecraft', 'Rocket League', 'League of Legends']
@@ -60,28 +94,29 @@ function CheckoutInfo() {
         <NavbarComponent data={{ background: 'transparent' }} />
         <Container fluid className="d-flex flex-column align-items-center p-0 m-0 mb-5">
           <h1 className="fw-bolder text-light text-center mt-3">DELTAKARINFORMASJON</h1>
+          {validate === true ? <p className="text-danger fw-bolder mt-3 m-0 my-auto p-0">Ein eller fleire av dei obligatoriske felta er ikkje fylte ut. Fyll ut dei resterande felta og prøv på nytt.</p> : null}
           <Container fluid className="d-flex flex-column align-items-center">
-            <Form noValidate validated={validated} className="checkout-form mt-5" onSubmit={submit}>
+            <Form className="checkout-form mt-5" onSubmit={submit}>
               <Row className="w-100 my-3">
                 <Form.Group controlId='formName' as={Col} lg={6}>
                   <Form.FloatingLabel label='NAMN'>
-                    <Form.Control required placeholder='NAMN' value={name} onChange={(e) => setName(e.target.value)} type='text' />
+                    <Form.Control placeholder='NAMN' value={name} onChange={(e) => setName(e.target.value)} type='text' />
                   </Form.FloatingLabel>
-                  <Form.Control.Feedback type='invalid'>Her er noko feil!</Form.Control.Feedback>
+                  {name === '' && validate === true ? <p className="text-danger mt-2">Dette feltet er ikkje fylt ut.</p> : null}
                 </Form.Group>
                 <Form.Group controlId='formPhone' as={Col} lg={6}>
                   <Form.FloatingLabel label='TELEFONNUMMER'>
-                    <Form.Control required placeholder='TELEFONNUMMER' value={phone} onChange={(e) => setPhone(e.target.value)} type='tel' />
+                    <Form.Control placeholder='TELEFONNUMMER' value={phone} onChange={(e) => setPhone(e.target.value)} type='tel' />
                   </Form.FloatingLabel>
-                  <Form.Control.Feedback type='invalid'>Her er noko feil!</Form.Control.Feedback>
+                  {phone === '' && validate === true ? <p className="text-danger mt-2">Dette feltet er ikkje fylt ut.</p> : null}
                 </Form.Group>
               </Row>
               <Row className="w-100 my-3">
                 <Form.Group controlId='formEmail' as={Col} lg={12}>
                   <Form.FloatingLabel label='E-POSTADRESSE'>
-                    <Form.Control required placeholder='E-POSTADRESSE' value={email} onChange={(e) => setEmail(e.target.value)} type='email' />
+                    <Form.Control placeholder='E-POSTADRESSE' value={email} onChange={(e) => setEmail(e.target.value)} type='email' />
                   </Form.FloatingLabel>
-                  <Form.Control.Feedback type='invalid'>Her er noko feil!</Form.Control.Feedback>
+                  {email === '' && validate === true ? <p className="text-danger mt-2">Dette feltet er ikkje fylt ut.</p> : null}
                 </Form.Group>
               </Row>
               <Row className="w-100">
@@ -90,31 +125,31 @@ function CheckoutInfo() {
               <Row className="w-100 my-3 mt-5">
                 <Form.Group controlId="formAddress" as={Col} lg={12}>
                   <Form.FloatingLabel label='ADRESSE'>
-                    <Form.Control required placeholder='ADRESSE' value={address} onChange={(e) => setAddress(e.target.value)} type='text' />
+                    <Form.Control placeholder='ADRESSE' value={address} onChange={(e) => setAddress(e.target.value)} type='text' />
                   </Form.FloatingLabel>
-                  <Form.Control.Feedback type='invalid'>Her er noko feil!</Form.Control.Feedback>
+                  {address === '' && validate === true ? <p className="text-danger mt-2">Dette feltet er ikkje fylt ut.</p> : null}
                 </Form.Group>
               </Row>
               <Row className="w-100 my-3">
                 <Form.Group controlId='formZip' as={Col} lg={3}>
                   <Form.FloatingLabel label='POSTNUMMER'>
-                    <Form.Control required placeholder='POSTNUMMER' value={zipcode} onChange={(e) => setZipcode(e.target.value)} type='text' />
+                    <Form.Control placeholder='POSTNUMMER' value={zipcode} onChange={(e) => setZipcode(e.target.value)} type='text' />
                   </Form.FloatingLabel>
-                  <Form.Control.Feedback type='invalid'>Her er noko feil!</Form.Control.Feedback>
+                  {zipcode === '' && validate === true ? <p className="text-danger mt-2">Dette feltet er ikkje fylt ut.</p> : null}
                 </Form.Group>
                 <Form.Group controlId='formCity' as={Col} lg={9}>
                   <Form.FloatingLabel label='POSTSTAD'>
-                    <Form.Control required placeholder='POSTSTAD' value={city} onChange={(e) => setCity(e.target.value)} type='text' />
+                    <Form.Control placeholder='POSTSTAD' value={city} onChange={(e) => setCity(e.target.value)} type='text' />
                   </Form.FloatingLabel>
-                  <Form.Control.Feedback type='invalid'>Her er noko feil!</Form.Control.Feedback>
+                  {city === '' && validate === true ? <p className="text-danger mt-2">Dette feltet er ikkje fylt ut.</p> : null}
                 </Form.Group>
               </Row>
               <Row className="w-100 my-3">
                 <Form.Group controlId='formCountry' as={Col} lg={12}>
                   <Form.FloatingLabel label='LAND'>
-                    <Form.Control required placeholder='LAND' value={country} onChange={(e) => setCountry(e.target.value)} type='text' />
+                    <Form.Control placeholder='LAND' value={country} onChange={(e) => setCountry(e.target.value)} type='text' />
                   </Form.FloatingLabel>
-                  <Form.Control.Feedback type='invalid'>Her er noko feil!</Form.Control.Feedback>
+                  {country === '' && validate === true ? <p className="text-danger mt-2">Dette feltet er ikkje fylt ut.</p> : null}
                 </Form.Group>
               </Row>
               <Row className="w-100">
@@ -123,18 +158,18 @@ function CheckoutInfo() {
               <Row className="w-100 my-3 mt-5">
                 <Form.Group controlId='formBirthdate' as={Col} lg={6}>
                   <Form.FloatingLabel label='FØDSELSDATO'>
-                    <Form.Control required value={birthdate} onChange={(e) => setBirthdate(e.target.value)} type='date' />
+                    <Form.Control value={birthdate} onChange={(e) => setBirthdate(e.target.value)} type='date' />
                   </Form.FloatingLabel>
-                  <Form.Control.Feedback type='invalid'>Her er noko feil!</Form.Control.Feedback>
+                  {birthdate === '' && validate === true ? <p className="text-danger mt-2">Dette feltet er ikkje fylt ut.</p> : null}
                 </Form.Group>
                 <Form.Group controlId='formGender' as={Col} lg={6}>
-                  <Form.Select required className="py-3" onSelect={(e) => setGender(e.target.value)}>
+                  <Form.Select className="py-3" onChange={(e) => setGender(e.target.value)}>
                     <option value=' '>VEL KJØNN</option>
                     <option value='Mann'>Mann</option>
                     <option value='Kvinne'>Kvinne</option>
                     <option value='Annet'>Annet</option>
                   </Form.Select>
-                  <Form.Control.Feedback type='invalid'>Her er noko feil!</Form.Control.Feedback>
+                  {gender === '' && validate === true ? <p className="text-danger mt-2">Dette feltet er ikkje fylt ut.</p> : null}
                 </Form.Group>
               </Row>
               <Row className="w-100">
@@ -147,23 +182,23 @@ function CheckoutInfo() {
                 <h3 className="fw-bolder text-light mx-4">Foresatt 1</h3>
                 <Form.Group controlId='formName2' as={Col} lg={6}>
                   <Form.FloatingLabel label='NAMN'>
-                    <Form.Control required placeholder='NAMN' value={parent1_name} onChange={(e) => setParent1Name(e.target.value)} type='text' />
+                    <Form.Control placeholder='NAMN' value={parent1_name} onChange={(e) => setParent1Name(e.target.value)} type='text' />
                   </Form.FloatingLabel>
-                  <Form.Control.Feedback type='invalid'>Her er noko feil!</Form.Control.Feedback>
+                  {parent1_name === '' && validate === true ? <p className="text-danger mt-2">Dette feltet er ikkje fylt ut.</p> : null}
                 </Form.Group>
                 <Form.Group controlId='formPhone2' as={Col} lg={6}>
                   <Form.FloatingLabel label='TELEFONNUMMER'>
-                    <Form.Control required placeholder='TELEFONNUMMER' value={parent1_phone} onChange={(e) => setParent1Phone(e.target.value)} type='tel' />
+                    <Form.Control placeholder='TELEFONNUMMER' value={parent1_phone} onChange={(e) => setParent1Phone(e.target.value)} type='tel' />
                   </Form.FloatingLabel>
-                  <Form.Control.Feedback type='invalid'>Her er noko feil!</Form.Control.Feedback>
+                  {parent1_phone === '' && validate === true ? <p className="text-danger mt-2">Dette feltet er ikkje fylt ut.</p> : null}
                 </Form.Group>
               </Row>
               <Row className="w-100 my-3">
                 <Form.Group controlId='formEmail2' as={Col} lg={12}>
                   <Form.FloatingLabel label='E-POSTADRESSE'>
-                    <Form.Control required placeholder='E-POSTADRESSE' value={parent1_email} onChange={(e) => setParent1Email(e.target.value)} type='email' />
+                    <Form.Control placeholder='E-POSTADRESSE' value={parent1_email} onChange={(e) => setParent1Email(e.target.value)} type='email' />
                   </Form.FloatingLabel>
-                  <Form.Control.Feedback type='invalid'>Her er noko feil!</Form.Control.Feedback>
+                  {parent1_email === '' && validate === true ? <p className="text-danger mt-2">Dette feltet er ikkje fylt ut.</p> : null}
                 </Form.Group>
               </Row>
               <Row className="w-100 my-3 mt-5">
@@ -189,14 +224,18 @@ function CheckoutInfo() {
               <Row className="w-100">
                 &nbsp;
               </Row>
-              <Row className="w-100 mt-3">
+              <Row className="w-100 mt-3 d-flex flex-column">
                 <h2 className="fw-bolder text-light">SPELINFORMASJON</h2>
+                <p className="text-light mt-2">
+                  Her kan du velja konkurransane du ynskjer å delta i under LANet. Du kan endra meining seinare.
+                </p>
               </Row>
               <Row className="w-100 my-3 mt-5 d-flex flex-column align-items-center">
                 <Form.Group as={Col} lg={12}>
                   {games.map((game) => {
                     return(
                       <Form.Check
+                        id={`${game}_${Math.random(1,100)}`}
                         label={game}
                         value={game}
                         type='checkbox'
@@ -221,6 +260,7 @@ function CheckoutInfo() {
                       <Form.FloatingLabel label='Brukarnamn - Fortnite'>
                         <Form.Control placeholder='Brukarnamn - Fortnite' type='text' value={fortnite} onChange={e => setFortnite(e.target.value)} />
                       </Form.FloatingLabel>
+                      {selectedGames.includes('Fortnite') && fortnite === '' ? <Form.Control.Feedback type='invalid'>Her er noko feil!</Form.Control.Feedback> : null}
                     </Form.Group>
                   </>
                 : null}
@@ -232,6 +272,7 @@ function CheckoutInfo() {
                       <Form.FloatingLabel label='Brukarnamn - Counter-Strike: Global Offensive'>
                         <Form.Control placeholder='Brukarnamn - Counter-Strike: Global Offensive' type='text' value={csgo} onChange={e => setCsgo(e.target.value)} />
                       </Form.FloatingLabel>
+                      {selectedGames.includes('Counter-Strike: Global Offensive') && csgo === '' ? <Form.Control.Feedback type='invalid'>Her er noko feil!</Form.Control.Feedback> : null}
                     </Form.Group>
                   </>
                 : null}
@@ -243,6 +284,7 @@ function CheckoutInfo() {
                       <Form.FloatingLabel label='Brukarnamn - Valorant'>
                         <Form.Control placeholder='Brukarnamn - Valorant' type='text' value={valorant} onChange={e => setValorant(e.target.value)} />
                       </Form.FloatingLabel>
+                      {selectedGames.includes('Valorant') && valorant === '' ? <Form.Control.Feedback type='invalid'>Her er noko feil!</Form.Control.Feedback> : null}
                     </Form.Group>
                   </>
                 : null}
@@ -254,6 +296,7 @@ function CheckoutInfo() {
                       <Form.FloatingLabel label='Brukarnamn - Forza Horizon 5'>
                         <Form.Control placeholder='Brukarnamn - Forza Horizon 5' type='text' value={fh5} onChange={e => setFh5(e.target.value)} />
                       </Form.FloatingLabel>
+                      {selectedGames.includes('Forza Horizon 5') && fh5 === '' ? <Form.Control.Feedback type='invalid'>Her er noko feil!</Form.Control.Feedback> : null}
                     </Form.Group>
                   </>
                 : null}
@@ -265,6 +308,7 @@ function CheckoutInfo() {
                       <Form.FloatingLabel label='Brukarnamn - Minecraft'>
                         <Form.Control placeholder='Brukarnamn - Minecraft' type='text' value={mc} onChange={e => setMc(e.target.value)} />
                       </Form.FloatingLabel>
+                      {selectedGames.includes('Minecraft') && mc === '' ? <Form.Control.Feedback type='invalid'>Her er noko feil!</Form.Control.Feedback> : null}
                     </Form.Group>
                   </>
                 : null}
@@ -276,6 +320,7 @@ function CheckoutInfo() {
                       <Form.FloatingLabel label='Brukarnamn - Rocket League'>
                         <Form.Control placeholder='Brukarnamn - Rocket League' type='text' value={rl} onChange={e => setRl(e.target.value)} />
                       </Form.FloatingLabel>
+                      {selectedGames.includes('Rocket League') && rl === '' ? <Form.Control.Feedback type='invalid'>Her er noko feil!</Form.Control.Feedback> : null}
                     </Form.Group>
                   </>
                 : null}
@@ -287,6 +332,7 @@ function CheckoutInfo() {
                       <Form.FloatingLabel label='Brukarnamn - League of Legends'>
                         <Form.Control placeholder='Brukarnamn - League of Legends' type='text' value={lol} onChange={e => setLol(e.target.value)} />
                       </Form.FloatingLabel>
+                      {selectedGames.includes('League of Legends') && lol === '' ? <Form.Control.Feedback type='invalid'>Her er noko feil!</Form.Control.Feedback> : null}
                     </Form.Group>
                   </>
                 : null}
