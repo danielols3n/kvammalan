b3e52675185824576a8bd53c6096445a23fdb420
danielols3n <73017930+danielols3n@users.noreply.github.com>
2024-04-09 20:21:00 +0200

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const config = {
-    apiKey: "AIzaSyDvcSL-RPBKhVRbxoYAwTxZhSMwhSAH0K4",
-    authDomain: "kvam-e-sport.firebaseapp.com",
-    projectId: "kvam-e-sport",
-    storageBucket: "kvam-e-sport.appspot.com",
-    messagingSenderId: "677296006230",
-    appId: "1:677296006230:web:19015e5496740617ab87f7",
-    measurementId: "G-KCCSKH771D"
+    
 }
 
 const app = initializeApp(config)
